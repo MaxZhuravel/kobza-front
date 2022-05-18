d29acc8a8ea283d678fabe306c50c6b73b1cd593
MaxZhuravel <93286002+MaxZhuravel@users.noreply.github.com>
2022-05-18 21:07:37 +0300

    add modal window of LogIn

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Nav, Navbar, Button, Container } from "react-bootstrap";
+import React, {useState} from 'react'
+import { Nav, Modal, Navbar, Button, Container, Form } from "react-bootstrap";
 import {Link, Outlet} from "react-router-dom";
 import styled from "styled-components";
 
@@ -14,6 +14,12 @@ const Styles = styled.div `
 `
 
 const Layout = () => {
+
+    const[show,setShow] = useState(false);
+
+    const handleClose=()=>setShow(false);
+    const handleShow=()=>setShow(true);
+
   return (
     <>
         <Styles>
@@ -29,13 +35,34 @@ const Layout = () => {
                         <Nav.Link><Link to="/about">Про нас</Link></Nav.Link>
                     </Nav>
                     <Nav>
-                        <Button variant="primary" className="me-2">Ввійти</Button>
-                        <Button variant="primary">Зареєструватись</Button>
+                        <Button variant="primary" className="me-2" onClick={handleShow}>Увійти</Button>
+                        <Button variant="primary" onClick={handleShow}>Зареєструватись</Button>
                     </Nav>
                 </Navbar.Collapse>
                 </Container>
             </Navbar>
         </Styles>
+        <Modal show = {show} onHide = {handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Увійти</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group controlId="fromBasicEmail">
+                        <Form.Label>Електронна адреса (email)</Form.Label>
+                        <Form.Control type="email" placeholder="Введіть email"/>
+                        <Form.Text className="text-muted">Ми ніколи не передаємо ваші дані третім особам</Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="fromBasicPassword">
+                        <Form.Label>Пароль</Form.Label>
+                        <Form.Control type="password" placeholder="Введіть пароль"/>
+                    </Form.Group>
+                    <Form.Group controlId="fromBasicCheckbox">
+                        <Form.Check type="checkbox" label="Remember me"/>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+        </Modal>
         <Outlet/>
         <footer>2022</footer>
     </>
