782b162ec48dc99b584f965ce329e85039a55adc
Taras Gez <taraspixel@gmail.com>
2022-05-19 15:46:19 +0300

    add axios & base REST

--- src/pages/ArticleList.jsx
+++ src/pages/ArticleList.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react";
+import { getArticles } from '../REST/articles';
+
 const ArticleList = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    loadArticles()
+  }, []);
+
+  function loadArticles() {
+    try {
+      getArticles().then((data) => setArticles(data?.results))
+    } catch (error) {
+      console.log('Load Articles error:', error)
+    }
+  }
+
+  const articlesMap = articles.map((article) => {
+    return <div>Name: {article.name}</div>
+  })
+
+  console.log('articles', articles)
+  
   return (
-    <h1>Список статей</h1>
+    <>
+      <h1>Список статей</h1>
+      <div>
+        {articlesMap}
+      </div>
+    </>
   )
 }
 
